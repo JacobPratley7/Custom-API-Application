8e7d6a41afb91956f3a62afeafddd64f2d155121
jpra3842 <jpra3842@uni.sydney.edu.au>
2021-05-23 20:20:00 +1000

    Added some functions to the InputFetcher interface

--- src/main/java/Model/InputAPI/InputFetcher.java
+++ src/main/java/Model/InputAPI/InputFetcher.java
@@ -1,5 +1,22 @@
 package Model.InputAPI;
 
+import Model.InputAPI.InputObjects.League;
+import org.json.simple.JSONObject;
+
+import java.util.List;
+
 public interface InputFetcher {
+
+    String getLeagues(String authToken);
+
+    JSONObject generateLeagueJSONObject(String jsonData);
+
+    String generateLeagueOutputString(JSONObject jsonObject);
+
+    String getSeries(String authToken, String leagueID);
+
+    JSONObject generateSeriesJSONObject(String jsonData);
+
+    String generateSeriesOutputString(JSONObject jsonObject);
 
 }
