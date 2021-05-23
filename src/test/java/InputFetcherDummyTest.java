06836606677dcd5c8c67d12011709461716ed007
jpra3842 <jpra3842@uni.sydney.edu.au>
2021-05-23 22:19:33 +1000

    added some test files, have not implemented tests yet

---
+++ src/test/java/InputFetcherDummyTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+public class InputFetcherDummyTest {
+
+    @Test
+    public void testInputFetcherDummyGetLeagues() {
+
+    }
+
+    @Test
+    public void testInputFetcherDummyGenerateLeagueJSONObject() {
+
+    }
+
+    @Test
+    public void testGenerateLeagueOutputString() {
+
+    }
+
+    @Test
+    public void testGetSeries() {
+
+    }
+
+    @Test
+    public void testGenerateSeriesJSONObject() {
+
+    }
+
+    @Test
+    public void testGenerateSeriesOutputString() {
+        
+    }
+}
