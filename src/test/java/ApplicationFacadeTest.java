78929870efad20f45830c19bae360ad8964c84e5
jpra3842 <jpra3842@uni.sydney.edu.au>
2021-05-28 22:48:55 +1000

    Implemented tests for getSeries in ApplicationFacade

--- src/test/java/ApplicationFacadeTest.java
+++ src/test/java/ApplicationFacadeTest.java
@@ -54,19 +54,61 @@ public class ApplicationFacadeTest {
         assertTrue(output.contains("url: "));
     }
 
-//    @Test
-//    public void testConvertToLeagueObject() {
-//        String output = "[{\"id\":4590,\"image_url\":\"https://cdn.pandascore.co/images/league/image/4590/600px-Oceanic_Esports.png\",\"modified_at\":\"2021-05-18T07:34:18Z\",\"name\":\"Hyperion x OEL Launch\",\"series\":[{\"begin_at\":\"2021-05-19T04:00:00Z\",\"description\":null,\"end_at\":\"2021-05-23T14:10:00Z\",\"full_name\":\"2021\",\"id\":3621,\"league_id\":4590,\"modified_at\":\"2021-05-23T18:55:54Z\",\"name\":null,\"season\":null,\"slug\":\"cs-go-hyperion-x-oel-launch-2021\",\"tier\":\"d\",\"winner_id\":126439,\"winner_type\":\"Team\",\"year\":2021}],\"slug\":\"cs-go-hyperion-x-oel-launch\",\"url\":\"\",\"videogame\":{\"current_version\":null,\"id\":3,\"name\":\"CS:GO\",\"slug\":\"cs-go\"}}]";
-//        JSONArray outputObjects = new JSONArray(output);
-//        JSONObject firstObject = (JSONObject )outputObjects.get(0);
-//        ApplicationFacade appFacade = new ApplicationFacade(null, null);
-//        League outputLeague = appFacade.convertToLeagueObject(firstObject);
-//        assertEquals("4590", outputLeague.getID());
-//        assertEquals("https://cdn.pandascore.co/images/league/image/4590/600px-Oceanic_Esports.png", outputLeague.getImageUrl());
-//        assertEquals("2021-05-18T07:34:18Z", outputLeague.getModifiedAt());
-//        assertEquals()
-//
-//
-//    }
+    @Test
+    public void testGetSeriesDataOnline() throws IOException {
+        InputFetcher imp = mock(InputFetcherImpl.class);
+        when(imp.getSeries(anyString(), anyString())).thenReturn("[{\"begin_at\":\"2021-05-19T04:00:00Z\",\"description\":null,\"end_at\":\"2021-05-23T14:10:00Z\",\"full_name\":\"2021\",\"id\":3621,\"league\":{\"id\":4590,\"image_url\":\"https://cdn.pandascore.co/images/league/image/4590/600px-Oceanic_Esports.png\",\"modified_at\":\"2021-05-18T07:34:18Z\",\"name\":\"Hyperion x OEL Launch\",\"slug\":\"cs-go-hyperion-x-oel-launch\",\"url\":\"\"},\"league_id\":4590,\"modified_at\":\"2021-05-23T18:55:54Z\",\"name\":null,\"season\":null,\"slug\":\"cs-go-hyperion-x-oel-launch-2021\",\"tier\":\"d\",\"tournaments\":[{\"begin_at\":\"2021-05-19T04:00:00Z\",\"end_at\":\"2021-05-23T14:10:00Z\",\"id\":6082,\"league_id\":4590,\"live_supported\":false,\"modified_at\":\"2021-05-24T08:29:01Z\",\"name\":\"Playoffs\",\"prizepool\":null,\"serie_id\":3621,\"slug\":\"cs-go-hyperion-x-oel-launch-2021-playoffs\",\"winner_id\":126439,\"winner_type\":\"Team\"}],\"videogame\":{\"id\":3,\"name\":\"CS:GO\",\"slug\":\"cs-go\"},\"videogame_title\":null,\"winner_id\":126439,\"winner_type\":\"Team\",\"year\":2021}]");
+        ApplicationFacade appFacade = new ApplicationFacade(imp, null);
+        String output = appFacade.getSeriesData("test", "randomSlug");
+        assertTrue(output.contains("begin at: 2021-05-19T04:00:00Z"));
+        assertTrue(output.contains("description: null"));
+        assertTrue(output.contains("end at: 2021-05-23T14:10:00Z"));
+        assertTrue(output.contains("full name: 2021"));
+        assertTrue(output.contains("id: 3621"));
+        assertTrue(output.contains("league id: 4590"));
+        assertTrue(output.contains("modified at: 2021-05-23T18:55:54Z"));
+        assertTrue(output.contains("name: null"));
+        assertTrue(output.contains("season: null"));
+        assertTrue(output.contains("slug: cs-go-hyperion-x-oel-launch-2021"));
+        assertTrue(output.contains("tier: d"));
+        assertTrue(output.contains("videogame title: null"));
+        assertTrue(output.contains("winner id: 126439"));
+        assertTrue(output.contains("winner type: Team"));
+        assertTrue(output.contains("year: 2021"));
+
+    }
+
+    @Test
+    public void testGetSeriesDataError() throws IOException {
+        InputFetcher imp = mock(InputFetcherImpl.class);
+        when(imp.getSeries(anyString(), anyString())).thenReturn("{\"error\":\"something went wrong\"}");
+        ApplicationFacade appFacade = new ApplicationFacade(imp, null);
+        String output = appFacade.getSeriesData("test", "test");
+        assertTrue(output.contains("error: something went wrong"));
+    }
+
+    @Test
+    public void testGetSeriesDataDummy() throws IOException {
+        InputFetcher imp = mock(InputFetcherDummy.class);
+        when(imp.getSeries(anyString(), anyString())).thenReturn("[{\"begin_at\":\"2021-05-19T04:00:00Z\",\"description\":null,\"end_at\":\"2021-05-23T14:10:00Z\",\"full_name\":\"2021\",\"id\":3621,\"league\":{\"id\":4590,\"image_url\":\"https://cdn.pandascore.co/images/league/image/4590/600px-Oceanic_Esports.png\",\"modified_at\":\"2021-05-18T07:34:18Z\",\"name\":\"Hyperion x OEL Launch\",\"slug\":\"cs-go-hyperion-x-oel-launch\",\"url\":\"\"},\"league_id\":4590,\"modified_at\":\"2021-05-23T18:55:54Z\",\"name\":null,\"season\":null,\"slug\":\"cs-go-hyperion-x-oel-launch-2021\",\"tier\":\"d\",\"tournaments\":[{\"begin_at\":\"2021-05-19T04:00:00Z\",\"end_at\":\"2021-05-23T14:10:00Z\",\"id\":6082,\"league_id\":4590,\"live_supported\":false,\"modified_at\":\"2021-05-24T08:29:01Z\",\"name\":\"Playoffs\",\"prizepool\":null,\"serie_id\":3621,\"slug\":\"cs-go-hyperion-x-oel-launch-2021-playoffs\",\"winner_id\":126439,\"winner_type\":\"Team\"}],\"videogame\":{\"id\":3,\"name\":\"CS:GO\",\"slug\":\"cs-go\"},\"videogame_title\":null,\"winner_id\":126439,\"winner_type\":\"Team\",\"year\":2021}]");
+        ApplicationFacade appFacade = new ApplicationFacade(imp, null);
+        String output = appFacade.getSeriesData("test", "randomSlug");
+        assertTrue(output.contains("begin at: 2021-05-19T04:00:00Z"));
+        assertTrue(output.contains("description: null"));
+        assertTrue(output.contains("end at: 2021-05-23T14:10:00Z"));
+        assertTrue(output.contains("full name: 2021"));
+        assertTrue(output.contains("id: 3621"));
+        assertTrue(output.contains("league id: 4590"));
+        assertTrue(output.contains("modified at: 2021-05-23T18:55:54Z"));
+        assertTrue(output.contains("name: null"));
+        assertTrue(output.contains("season: null"));
+        assertTrue(output.contains("slug: cs-go-hyperion-x-oel-launch-2021"));
+        assertTrue(output.contains("tier: d"));
+        assertTrue(output.contains("videogame title: null"));
+        assertTrue(output.contains("winner id: 126439"));
+        assertTrue(output.contains("winner type: Team"));
+        assertTrue(output.contains("year: 2021"));
+    }
+
 
 }
