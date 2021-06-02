fc8551c5dcff289d882acebd788d5e91adc96dce
jpra3842 <jpra3842@uni.sydney.edu.au>
2021-06-02 16:55:39 +1000

    GREEN - getLeagueData has been modified and now passes the refactored tests

--- src/main/java/Model/ApplicationFacade.java
+++ src/main/java/Model/ApplicationFacade.java
@@ -68,6 +68,13 @@ public class ApplicationFacade {
         newLeague.setSlug(leagueData.get("slug"));
         newLeague.setUrl(leagueData.get("url"));
         JSONArray series = leagueData.getJSONArray("series");
+        List<Series> leagueSeries = new ArrayList<>();
+        for(int i = 0; i < series.length(); i++) {
+            org.json.JSONObject currentSeriesInfo = (org.json.JSONObject) series.get(i);
+            Series currentSeries = convertToSeriesObjectSimple(currentSeriesInfo);
+            leagueSeries.add(currentSeries);
+        }
+        newLeague.setSeries(leagueSeries);
         org.json.JSONObject videoGame = leagueData.getJSONObject("videogame");
         newLeague.setVideoGameCurrentVersion(videoGame.get("current_version"));
         newLeague.setVideoGameName(videoGame.get("name"));
@@ -101,6 +108,26 @@ public class ApplicationFacade {
                 leagueOutput = (leagueOutput.concat("\nname: ")).concat(currentLeague.getName());
                 leagueOutput = (leagueOutput.concat("\nslug: ")).concat(currentLeague.getSlug());
                 leagueOutput = (leagueOutput.concat("\nurl: ")).concat(currentLeague.getUrl());
+                leagueOutput = leagueOutput.concat("\nseries:");
+                List<Series> currentSeries = currentLeague.getSeries();
+                for(Series s: currentSeries) {
+                    leagueOutput = (leagueOutput.concat("\n\tbegin at: ")).concat(s.getBeginAt());
+                    leagueOutput = (leagueOutput.concat("\n\tdescription: ")).concat(s.getDescription());
+                    leagueOutput = (leagueOutput.concat("\n\tend at: ")).concat(s.getEndAt());
+                    leagueOutput = (leagueOutput.concat("\n\tfull name: ")).concat(s.getFullName());
+                    leagueOutput = (leagueOutput.concat("\n\tid: ")).concat(s.getID());
+                    leagueOutput = (leagueOutput.concat("\n\tleague id: ")).concat(s.getLeagueId());
+                    leagueOutput = (leagueOutput.concat("\n\tmodified at: ")).concat(s.getModifiedAt());
+                    leagueOutput = (leagueOutput.concat("\n\tname: ")).concat(s.getName());
+                    leagueOutput = (leagueOutput.concat("\n\tseason: ")).concat(s.getSeason());
+                    leagueOutput = (leagueOutput.concat("\n\tslug: ")).concat(s.getSlug());
+                    leagueOutput = (leagueOutput.concat("\n\ttier: ")).concat(s.getTier());
+                    leagueOutput = (leagueOutput.concat("\n\twinner id: ")).concat(s.getWinnerId());
+                    leagueOutput = (leagueOutput.concat("\n\twinner type: ")).concat(s.getWinnerType());
+                    leagueOutput = (leagueOutput.concat("\n\tyear: ")).concat(s.getYear());
+                    leagueOutput = leagueOutput.concat("\n");
+                    leagueOutput = leagueOutput.concat("\n");
+                }
                 leagueOutput = leagueOutput.concat("\nvideo game:");
                 leagueOutput = (leagueOutput.concat("\n\tcurrent version: ")).concat(currentLeague.getVideoGameCurrentVersion());
                 leagueOutput = (leagueOutput.concat("\n\tid: ")).concat(currentLeague.getVideoGameID());
@@ -114,6 +141,25 @@ public class ApplicationFacade {
     }
 
 
+    private Series convertToSeriesObjectSimple(org.json.JSONObject seriesData) {
+        Series newSeries = new Series();
+        newSeries.setID(seriesData.get("id"));
+        newSeries.setBeginAt(seriesData.get("begin_at"));
+        newSeries.setDescription(seriesData.get("description"));
+        newSeries.setEndAt(seriesData.get("end_at"));
+        newSeries.setFullName(seriesData.get("full_name"));
+        newSeries.setLeagueID(seriesData.get("league_id"));
+        newSeries.setModifiedAt(seriesData.get("modified_at"));
+        newSeries.setName(seriesData.get("name"));
+        newSeries.setSeason(seriesData.get("season"));
+        newSeries.setSlug(seriesData.get("slug"));
+        newSeries.setTier(seriesData.get("tier"));
+        newSeries.setWinnerId(seriesData.get("winner_id"));
+        newSeries.setWinnerType(seriesData.get("winner_type"));
+        newSeries.setYear(seriesData.get("year"));
+
+        return newSeries;
+    }
 
     private Series convertToSeriesObject(org.json.JSONObject seriesData) {
         Series newSeries = new Series();
