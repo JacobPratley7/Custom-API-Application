b4f8cafb88abd4d3c60610f0276cf7221fb7082a
jpra3842 <jpra3842@uni.sydney.edu.au>
2021-06-17 22:52:11 +1000

    RED: ApplicationFacade tests do not pass

--- src/test/java/ApplicationFacadeTest.java
+++ src/test/java/ApplicationFacadeTest.java
@@ -590,8 +590,9 @@ public class ApplicationFacadeTest {
 
         appFacade.getSeriesData( "test");
         String output = appFacade.sendReport();
+        System.out.println(output);
         assertTrue(output.contains("Message sent Successfully!"));
-        assertTrue(output.contains("*League"));
+        assertTrue(output.contains("*league"));
     }
 
     @Test
@@ -783,7 +784,7 @@ public class ApplicationFacadeTest {
         appFacade.getLeagueData(false);
         when(dbManager.retrieveData()).thenReturn("[{\"id\":5678,\"image_url\":\"https://cdn.pandascore.co/images/league/image/4590/600px-Oceanic_Esports.png\",\"modified_at\":\"2021-05-18T07:34:18Z\",\"name\":\"Hyperion x OEL Launch\",\"series\":[{\"begin_at\":\"2021-05-19T04:00:00Z\",\"description\":null,\"end_at\":\"2021-05-23T14:10:00Z\",\"full_name\":\"2021\",\"id\":3621,\"league_id\":4590,\"modified_at\":\"2021-05-23T18:55:54Z\",\"name\":null,\"season\":null,\"slug\":\"cs-go-hyperion-x-oel-launch-2021\",\"tier\":\"d\",\"winner_id\":126439,\"winner_type\":\"Team\",\"year\":2021}],\"slug\":\"cs-go-hyperion-x-oel-launch\",\"url\":\"\",\"videogame\":{\"current_version\":null,\"id\":3,\"name\":\"CS:GO\",\"slug\":\"cs-go\"}}]");
         String nextOutput = appFacade.getLeagueData(true);
-        assertTrue(output.contains("Too New"));
+        assertTrue(nextOutput.contains("Too New"));
         assertTrue(nextOutput.contains("id: 5678"));
         assertTrue(nextOutput.contains("image url: https://cdn.pandascore.co/images/league/image/4590/600px-Oceanic_Esports.png"));
         assertTrue(nextOutput.contains("modified at: 2021-05-18T07:34:18Z"));
